5ea70dc93f23ed9ab8b66b344f27925815f04341
ubk8751 <ubk8751@gmail.com>
2021-11-26 12:43:52 +0100

    Create gay-fifo.c

---
+++ gay-fifo.c
@@ -0,0 +1,63 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+  if(argc != 4) {
+    printf("U suc, pls use 3 args\n");
+    exit(1);
+  }
+  int page_count = atoi(argv[1]);
+  int page_size = atoi(argv[2]);
+  char* file_name = argv[3];
+  printf("No of physical pages = %d, page size = %d\nReading memory trace from %s\n", page_count, page_size, file_name);
+
+  // hippedy hoppedy this code is now our property
+  FILE * fp;
+  char * line = NULL;
+  size_t len = 0;
+  ssize_t read;
+
+  int read_lines = 0;
+  int page_faults = 0;
+
+  int table[page_count];
+
+  for(int i =0; i < page_count; i++){
+    table[i] = -1;
+  }
+
+  fp = fopen(file_name, "r");
+  if (fp == NULL)
+      exit(EXIT_FAILURE);
+
+  long read_number;
+  int final_addr;
+  int temp;
+  while ((read = getline(&line, &len, fp)) != -1) {
+      read_lines++;
+      read_number = atoi(line);
+
+      temp = (int)(read_number/page_size);
+      final_addr = temp*page_size;
+
+      int found = 0;
+      for (int i = 0; i < page_count; i++) {
+        if (table[(i)] == final_addr) {
+          found = 1;
+        }
+      }
+      if(found == 0){
+        page_faults++;
+        table[(read_lines % page_count)] = final_addr;
+      }
+  }
+  fclose(fp);
+  if (line)
+      free(line);
+
+  printf("Read %d memory references => %d pagefaults\n", read_lines, page_faults);
+
+  return 0;
+}
