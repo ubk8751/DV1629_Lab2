3d18d13011ee4f5885f85b039ddff36839014ee2
Emil Karlström <emil.karlstrom@gmail.com>
2021-12-01 15:27:41 +0100

    Fucking comments

--- gay-optimal.c
+++ gay-optimal.c
@@ -12,27 +12,27 @@ typedef struct {
 
 int main(int argc, char** argv)
 {
-  if(argc != 4) {
-    printf("U suc, pls use 3 args\n");
-    exit(1);
-  }
-  const int page_count = atoi(argv[1]);
-  const int page_size = atoi(argv[2]);
-  char* file_name = argv[3];
-  printf("No of physical pages = %d, page size = %d\nReading memory trace from %s\n", page_count, page_size, file_name);
+    if(argc != 4) {
+        printf("U suc, pls use 3 args\n");
+        exit(1);
+    }
+    const int page_count = atoi(argv[1]);
+    const int page_size = atoi(argv[2]);
+    char* file_name = argv[3];
+    printf("No of physical pages = %d, page size = %d\nReading memory trace from %s\n", page_count, page_size, file_name);
 
-  // hippedy hoppedy this code is now our property
-  FILE * fp;
-  char * line = NULL;
-  size_t len = 0;
-  ssize_t read;
+    // hippedy hoppedy this code is now our property
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
 
-  fp = fopen(file_name, "r");
-  if (fp == NULL)
-      exit(EXIT_FAILURE);
+    fp = fopen(file_name, "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
 
-  long read_number;
-  int final_addr;
+    long read_number;
+    int final_addr;
 
     printf("Allocating very chonky array... ");
     long mem_addrs[MEMORY_TRACE_LENGTH];
@@ -53,63 +53,65 @@ int main(int argc, char** argv)
         table[i].next_use = 0;
     }
 
-  fclose(fp);
-  if (line)
-      free(line);
+    fclose(fp);
+    if (line)
+        free(line);
 
-  int page_faults = 0, size = 0;
-  for(int i = 0; i < MEMORY_TRACE_LENGTH; i++) {
-    final_addr = mem_addrs[i];
+    int page_faults = 0, size = 0;
+    for(int i = 0; i < MEMORY_TRACE_LENGTH; i++) {
+        final_addr = mem_addrs[i];
 
-    int found = 0;
-    for (int j = 0; j < page_count; j++) {
-        if (table[j].addr == final_addr) {
-            found = 1;
-            break;
+        // Search if page is loaded in table
+        int found = 0;
+        for (int j = 0; j < page_count; j++) {
+            if (table[j].addr == final_addr) {
+                found = 1; break;
+            }
         }
-    }
-    if(found == 0){
-        if(size < page_count){
-            table[size].addr = final_addr;
-            page_faults++;
-            size++;
-        } else {
-            for(int k = 0; k < page_count; k++)
-                table[k].next_use = i;
 
-            for(int k = 0; k < page_count; k++){
-                for(int j = i+1; j < MEMORY_TRACE_LENGTH; j++){
-                    if(table[k].addr == mem_addrs[j]){
-                        table[k].next_use = j;
-                        break;
+        // If page is not found, this is a page fault
+        if(found == 0){
+            page_faults++;
+
+            // If page table is not full, add page to table
+            if(size < page_count){
+                table[size].addr = final_addr;
+                size++;
+            } // Else replace page with Optimal Page Replacement Algorithm
+            else {
+                int latest_use = 0, latest_use_index = -1;
+
+                // Loop through all loaded pages
+                for(int k = 0; k < page_count; k++){
+                    table[k].next_use = -1; // Reset the data to -1
+
+                    // Loop through all addresses in the future
+                    for(int j = i+1; j < MEMORY_TRACE_LENGTH; j++){
+
+                        // If address in page is same as address in future, store the time stamp
+                        if(table[k].addr == mem_addrs[j]){
+                            table[k].next_use = j; break;
+                        }
+                    }
+
+                    // If page is not to be used, replace it
+                    if(table[k].next_use == -1){
+                        latest_use_index = k; break;
+                    } 
+                    // Else find the page which's next use is the latest of all loaded pages
+                    else {
+                        if(latest_use < table[k].next_use){
+                            latest_use = table[k].next_use;
+                            latest_use_index = k;
+                        }
                     }
                 }
+                table[latest_use_index].addr = final_addr;
             }
-            int latest_use = 0, latest_use_index = -1;
-            for(int k = 0; k < page_count; k++){
-                if(table[k].next_use == i){
-                    latest_use_index = k;
-                    break;
-                } else {
-                    if(latest_use < table[k].next_use){
-                        latest_use = table[k].next_use;
-                        latest_use_index = k;
-                    }
-                }
-            }
-            
-            for(int k = 0; k < page_count; k++){
-              printf("%d: %d (%d) next use: %d\n", k, table[k].addr, table[k].next_use - i, table[k].next_use);
-            }
-            printf("Step #%d: Replacing page #%d (%d) due to next use on step %d (%lu)\n", i, latest_use_index, table[latest_use_index].addr, latest_use, mem_addrs[latest_use]);
-            printf("\n");
-            table[latest_use_index].addr = final_addr;
-            page_faults++;
         }
     }
-  }
 
-  printf("Read %d memory references => %d pagefaults\n\n", read_lines, page_faults);
+    printf("Read %d memory references => %d pagefaults\n\n", read_lines, page_faults);
 
-  return 0;
+    return 0;
 }
